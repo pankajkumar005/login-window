53065afbbb7b711d7df5d78a9e8cdb79142f2891
unknown <pankajkumar005@gmail.com>
2015-09-11 15:43:13 +0530

    Adding java documentation and support for java docs.#

--- src/main/java/com/pankaj/api/Users.java
+++ src/main/java/com/pankaj/api/Users.java
@@ -19,15 +19,48 @@ import java.io.FileReader;
 import java.io.IOException;
 import com.pankaj.api.UserService;
 
+/**
+* @author Pankaj Kumar
+*/
  
 @Path("/users")
 public class Users {
 
-		
+
+	/**
+	 * This method is for registering the user information. This is
+	 * a REST api with POST method. The url path is "users/register"
+	 * and the data to be posted is in json format. For example, if the request is being 
+	 * posted via jQuery:
+	 * <p>
+	 * <pre>
+	 * {@code
+	 * $.ajax({ 
+     *          type        :   'POST'  ,
+     *          url         :   '/api/users/register',
+     *          data        :   JSON.stringify({
+     *                          	userName: <someName>,
+     *                              password: <pwd>,
+     *                            	email: <email@dom.suf>
+     *                         }),
+     *          contentType :   'application/json',
+     *          dataType    :   'json',
+     *      	success     :   function(resp) {
+*							if(!resp.error) {
+*								//Registration Successfull
+	*						}
+     *                        }
+     *});
+     * }
+     * </pre>
+     * <p> The response returned, will be blank in case of successfull user registration.
+     * Otherwise, it contains 'error' field as true and 'message' field, containing the relevent error message.
+	 * 
+ 	*/	
 	@Path("/register")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response writeUserName(String requestObject) throws IOException, JSONException {
+	public Response writeUserInfo(String requestObject) throws IOException, JSONException {
 		
 		JSONObject jsonObject = new JSONObject(requestObject);
 		JSONObject resultObject = new JSONObject();
@@ -54,6 +87,28 @@ public class Users {
 		return Response.status(200).entity(result).build();
 	}
 
+	/**
+	 * This method is for checking if the given user name is registered or not. This is
+	 * a REST api with GET method. The url path is "users/check". For example, if the request is being 
+	 * posted via jQuery:
+	 * <p>
+	 * <pre>
+	 * {@code
+	 * $.get('/RESTfulExample/api/users/check?name=someName', function(data){
+    *    		if(data.found) {
+	*				// User name is registered
+    *    		} else {
+	*				// User name is not registered
+    *			}
+    *    
+    *  });
+     * }
+     * </pre>
+     * <p> The response returned, contains 'found' field as true if user is registered
+     * or false if user is not registered.
+	 * 
+ 	*/
+
 	@Path("/check")
 	@GET
 	@Produces("application/json")
@@ -66,6 +121,43 @@ public class Users {
 		String result = "" + jsonObject;
 		return Response.status(200).entity(result).build(); 		
 	}
+
+	/**
+	 * This method is for validating the user credentials. This is
+	 * a REST api with POST method. The url path is "users/validate"
+	 * and the data to be posted is in json format. For example, if the request is being 
+	 * posted via jQuery:
+	 * <p>
+	 * <pre>
+	 * {@code
+	 * $.ajax({ 
+     *          type        :   'POST'  ,
+     *          url         :   '/api/users/register',
+     *          data        :   JSON.stringify({
+     *                          	userName: <someName>,
+     *                              password: <pwd>
+     *                         }),
+     *          contentType :   'application/json',
+     *          dataType    :   'json',
+     *      	success     :   function(resp) {
+	*						if(!resp.error) {
+	*							if(resp.valid) {
+	*								//registration successfull
+	*							} else {
+	*								// Login failure, credentials wrong
+	*							}
+	*						} else {
+	*						// Login failure, see resp.message
+	*						}
+     *                   }
+     *});
+     * }
+     * </pre>
+     * <p> The response returned, will have filed 'valid' as true in case of successfull 
+     * login. In case of error, it contains 'error' field
+     * as true and 'message' field, containing the relevent error message.
+	 * 
+ 	*/
 
 	@Path("/validate")
 	@POST
